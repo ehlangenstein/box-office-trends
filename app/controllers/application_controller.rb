ebb5b694873f6c907ea5a9ec5f41460c32bcd216
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-11-06 05:37:50 +0000

    cleaning up companies functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
+  before_action :set_top_companies
 
   def current_user
     @current_user = User.find_by({ "id" => session["user_id"] })
   end
+
+  private
+
+  def set_top_companies
+    top_company_ids = [7, 41077, 14] # Replace with actual company IDs you want to track
+    @top_companies = Company.where(id: top_company_ids)
+  end
+
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -35,49 +35,12 @@ class CompaniesController < ApplicationController
     end
   end
 
-    # If the company is Neon, fetch the list of movies using the scraper
-    if @company.company_id == 90733
-      @neon_movies = NeonFilmsWikiScraper.new.fetch_movies
-      Rails.logger.info "Fetched #{@neon_movies.size} movies from Wikipedia."
-      
-      @movies_data = @neon_movies.map do |movie|
-        # Use the updated TmdbWikidataService to fetch both TMDB and IMDb IDs
-        movie_ids = TmdbWikidataService.fetch_movie_from_wikidata_id(movie[:wikidata_id])
-      
-        if movie_ids 
-          imdb_id = movie_ids[:imdb_id]
-          tmdb_id = movie_ids[:tmdb_id]
-          box_office_data = fetch_box_office_data(imdb_id) if imdb_id
-          {
-            title: movie[:title],
-            tmdb_id: tmdb_id,
-            imdb_id: imdb_id,
-            box_office_data: box_office_data
-          }
-        else
-          {
-            title: movie[:title],
-            tmdb_id: 'N/A',
-            imdb_id: 'N/A',
-            box_office_data: 'N/A'
-          }
-        end
-      end
-    else
-      @neon_movies = [] # Default for companies without a movie list from Wikipedia
-      Rails.logger.info "IDK"
-    end
 
 
      # Find the movies associated with the company in the logged movies table
-    @movies = Movie.joins(:production_companies).where(production_companies: { prodco_id: @company.company_id }) 
+    #@movies = Movie.joins(:production_companies).where(production_companies: { prodco_id: @company.company_id }) 
 
-     # Fetch the movies from TMDB API
-     fetch_company_movies_from_api(@company.company_id)
   end 
-
-
-
 
   def search
     if params[:query].present?
@@ -85,16 +48,10 @@ class CompaniesController < ApplicationController
     end
   end
 
-
-
-
-
   def top_companies
-    @top_companies = Company.top_companies
+    top_company_ids = [7, 41077, 14] # Replace with actual company IDs you want to track
+    @top_companies = Company.where(id: top_company_ids)
   end
-
-
-
 
   def add_company
     company_data = fetch_company_details_from_tmdb(params[:id])
@@ -139,6 +96,7 @@ class CompaniesController < ApplicationController
       Rails.logger.error "No companies found for query: #{query}"
     end
   end
+
   def fetch_company_details_from_tmdb(company_id)
     url = URI("#{TMDB_API_URL}/company/#{company_id}?api_key=#{TMDB_API_KEY}")
 
@@ -156,37 +114,4 @@ class CompaniesController < ApplicationController
       nil
     end
   end
- 
-  # Fetch the movies from the TMDB API for a given company ID
-  def fetch_company_movies_from_api(company_id)
-    @api_movies = []
-    page = 1
-    total_pages = 1  # Default value for the loop to start
-# Loop through all pages of results
-while page <= total_pages do
-  url = URI("#{TMDB_API_URL}/discover/movie?with_companies=#{company_id}&api_key=#{TMDB_API_KEY}&page=#{page}")
-  Rails.logger.info "Fetching page #{page} of movies from TMDB for company ID: #{company_id}"
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-
-  request = Net::HTTP::Get.new(url)
-  request["accept"] = 'application/json'
-
-  response = http.request(request)
-  api_movies_data = JSON.parse(response.body)
-
-  if response.code == "200" && api_movies_data["results"]
-    @api_movies.concat(api_movies_data["results"])  # Append the results to the @api_movies array
-    total_pages = api_movies_data["total_pages"]    # Update total pages from API response
-    page += 1                                       # Move to the next page
-  else
-    Rails.logger.error "Error fetching movies: #{response.body}"
-    break  # Exit the loop in case of an error
-  end
 end
-
-Rails.logger.info "Fetched #{@api_movies.size} movies from TMDB for company ID: #{company_id}"
-end
-
-end 
